4f8570dd0ad86f13fd91c757af337273163c4253
Alex Novak <alexwnovak@gmail.com>
2018-01-01 23:02:06 -0500

    +Pending feature for CmdletHost
    Fleshes out one scenario that runs a pre-programmed cmdlet and asserts that it returns the value we expect.

---
+++ src/Lodestone.PowerShell.AcceptanceTests/CmdletHostFeature.feature
@@ -0,0 +1,9 @@
+﻿Feature: CmdletHost Feature
+   As a PowerShell cmdlet author
+   I want to run my cmdlet in isolation
+   So I can ensure that all the components integrate correctly
+
+@Acceptance
+Scenario: Running a cmdlet with a simple return value
+   When I run host the ReturnFive cmdlet
+   Then the it should return 5
---
+++ src/Lodestone.PowerShell.AcceptanceTests/Steps/CmdletHostFeatureSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Lodestone.PowerShell.AcceptanceTests.Steps
+{
+   [Binding]
+   public class CmdletHostFeatureSteps
+   {
+      [When( @"I run host the ReturnFive cmdlet" )]
+      public void WhenIRunHostTheReturnFiveCmdlet()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Then( @"the it should return (.*)" )]
+      public void ThenTheItShouldReturn( int expectedReturnValue )
+      {
+         ScenarioContext.Current.Pending();
+      }
+   }
+}
