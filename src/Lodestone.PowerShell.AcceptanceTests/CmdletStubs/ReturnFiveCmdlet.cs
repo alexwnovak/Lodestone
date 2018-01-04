f676112cdc96b77298377844db5c34be7a442fcc
Alex Novak <alexwnovak@gmail.com>
2018-01-03 22:13:30 -0500

    +ReturnFiveCmdlet stub
    We'll use this to push a simple but clear cmdlet through the host we're testing to ensure it can coordinate with PowerShell.

---
+++ src/Lodestone.PowerShell.AcceptanceTests/CmdletStubs/ReturnFiveCmdlet.cs
@@ -0,0 +1,13 @@
+﻿using System.Management.Automation;
+
+namespace Lodestone.PowerShell.AcceptanceTests.CmdletStubs
+{
+   [Cmdlet( VerbsCommon.Get, "Five" )]
+   internal class ReturnFiveCmdlet : Cmdlet
+   {
+      protected override void ProcessRecord()
+      {
+         WriteObject( 5 );
+      }
+   }
+}
