e8aa114f1f6e06b5a8ac45c389b735ad71bf882c
Alex Novak <alexwnovak@gmail.com>
2018-01-03 22:21:54 -0500

    +For<> method to start a cmdlet host flow

--- src/Lodestone.PowerShell/CmdletHost.cs
+++ src/Lodestone.PowerShell/CmdletHost.cs
@@ -1,6 +1,10 @@
-﻿namespace Lodestone.PowerShell
+﻿using System.Management.Automation;
+
+namespace Lodestone.PowerShell
 {
-   public class CmdletHost
+   public static class CmdletHost
    {
+      public static HostFlow<TCmdletType> For<TCmdletType>() where TCmdletType : Cmdlet =>
+         new HostFlow<TCmdletType>();
    }
 }
--- src/Lodestone.PowerShell/HostFlow.cs
+++ src/Lodestone.PowerShell/HostFlow.cs
@@ -1,6 +1,11 @@
-﻿namespace Lodestone.PowerShell
+﻿using System.Management.Automation;
+
+namespace Lodestone.PowerShell
 {
-   public class HostFlow
+   public class HostFlow<TCmdletType> where TCmdletType : Cmdlet
    {
+      internal HostFlow()
+      {
+      }
    }
 }
