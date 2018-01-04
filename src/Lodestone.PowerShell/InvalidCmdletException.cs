14bcbb41db52bf1f5d377eb608a2c18e713280a0
Alex Novak <alexwnovak@gmail.com>
2018-01-03 22:37:07 -0500

    +InvalidCmdletException
    We'll use this as a communication funnel to throw errors out of the library to the consumers. This can shield real PowerShell exceptions and lets us throw a common language around.

---
+++ src/Lodestone.PowerShell/InvalidCmdletException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Lodestone.PowerShell
+{
+   [Serializable]
+   public class InvalidCmdletException : Exception
+   {
+      public InvalidCmdletException()
+      {
+      }
+
+      public InvalidCmdletException( string message ) : base( message )
+      {
+      }
+
+      public InvalidCmdletException( string message, Exception inner )
+         : base( message, inner )
+      {
+      }
+
+      protected InvalidCmdletException( SerializationInfo info, StreamingContext context )
+         : base( info, context )
+      {
+      }
+   }
+}
