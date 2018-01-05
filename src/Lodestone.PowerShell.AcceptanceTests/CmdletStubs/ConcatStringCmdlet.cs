b6a214c7c8657ac8a7aff4d1936dd96ac3e5fd79
Alex Novak <alexwnovak@gmail.com>
2018-01-04 22:27:08 -0500

    +Cmdlet for accepting a parameter

---
+++ src/Lodestone.PowerShell.AcceptanceTests/CmdletStubs/ConcatStringCmdlet.cs
@@ -0,0 +1,19 @@
+﻿using System.Management.Automation;
+
+namespace Lodestone.PowerShell.AcceptanceTests.CmdletStubs
+{
+   [Cmdlet( "Concat", "String" )]
+   public class ConcatStringCmdlet : Cmdlet
+   {
+      public string Value
+      {
+         get;
+         set;
+      }
+
+      protected override void ProcessRecord()
+      {
+         WriteObject( $"Received {Value}" );
+      }
+   }
+}
