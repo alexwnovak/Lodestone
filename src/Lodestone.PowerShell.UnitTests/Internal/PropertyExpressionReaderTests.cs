e81666174bbd690542bf3e12c0ffcc31919e9165
Alex Novak <alexwnovak@gmail.com>
2018-01-05 22:20:50 -0500

    +PropertyExpressionReader
    We'll use this to extract out the property name used in the With<> form when referring to a property name. We'll also use it to validate the correct form, to make sure it's formed the way we need.

---
+++ src/Lodestone.PowerShell.UnitTests/Internal/PropertyExpressionReaderTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using Lodestone.PowerShell.Internal;
+
+namespace Lodestone.PowerShell.UnitTests.Internal
+{
+   public class PropertyExpressionReaderTests
+   {
+      [Fact]
+      public void GetName_ExpressionRefersToProperty_GetPropertyName()
+      {
+         string name = PropertyExpressionReader.Validate<DateTime, int>( dt => dt.Hour );
+
+         name.Should().Be( nameof( DateTime.Hour ) );
+      }
+   }
+}
---
+++ src/Lodestone.PowerShell/Internal/PropertyExpressionReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Lodestone.PowerShell.Internal
+{
+   internal static class PropertyExpressionReader
+   {
+      public static string Validate<T, TParameterType>( Expression<Func<T, TParameterType>> property )
+      {
+         var member = (MemberExpression) property.Body;
+         return member.Member.Name;
+      }
+   }
+}
