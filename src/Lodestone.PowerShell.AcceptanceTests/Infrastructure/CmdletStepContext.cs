3e365e01fd7b4011d731a0107b2883a4e6dab205
Alex Novak <alexwnovak@gmail.com>
2018-01-05 21:01:55 -0500

    +CmdletStepContext to help invoke CmdletHost
    There's an inherent conflict from the CmdletHost API and reusing steps. For CmdletHost, you commit up front to what kind of Cmdlet you're hosting--and then you use that type throughout the invocation. To reuse steps though, we need to be fluid with what kind of Cmdlet we're hosting, since it can change between scenarios. There's an impedance here.
    
    This class tries to bridge the gap (without reflection!), by allowing the steps to not ENTIRELY commit up front to the Cmdlet they're using. Instead, this class adapts the host API and adds wiggle room for changing what Cmdlet type it is.

---
+++ src/Lodestone.PowerShell.AcceptanceTests/Infrastructure/CmdletStepContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Management.Automation;
+
+namespace Lodestone.PowerShell.AcceptanceTests.Infrastructure
+{
+   internal class CmdletStepContext
+   {
+      private object _hostFlow;
+      private Func<object> _runAction;
+
+      public void For<TCmdletType>() where TCmdletType : Cmdlet
+      {
+         _hostFlow = CmdletHost.For<TCmdletType>();
+         _runAction = () => ( (HostFlow<TCmdletType>) _hostFlow ).Run();
+      }
+
+      public void With<TCmdletType, TParameterType>( Expression<Func<TCmdletType, TParameterType>> property, TParameterType value )
+         where TCmdletType : Cmdlet
+      {
+         ( (HostFlow<TCmdletType>) _hostFlow ).With( property, value );
+      }
+
+      public object Run()
+      {
+         return _runAction();
+      }
+   }
+}
